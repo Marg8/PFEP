012bec48a072ae2f0e999cb276d91dffd16d2722
Marg8 <50607688+Marg8@users.noreply.github.com>
2026-08-28 18:28:05 +0000

    DB Local config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST',    getenv('DB_HOST')    ?: 'mtapp05');
+define('DB_HOST',    getenv('DB_HOST')    ?: 'localhost');
 define('DB_PORT',    getenv('DB_PORT')    ?: '3306');
 define('DB_NAME',    getenv('DB_NAME')    ?: 'materials');
-define('DB_USER',    getenv('DB_USER')    ?: 'mrodriguez');
-define('DB_PASS',    getenv('DB_PASS')    ?: '');
+define('DB_USER',    getenv('DB_USER')    ?: 'root');
+define('DB_PASS',    getenv('DB_PASS') !== false ? getenv('DB_PASS') : 'L3aNnM43@ja!');
 define('DB_SOCKET',  getenv('DB_SOCKET')  ?: '');
 define('DB_CHARSET', 'utf8mb4');
 
